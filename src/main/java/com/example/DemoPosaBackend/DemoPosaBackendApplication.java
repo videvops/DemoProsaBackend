b00bab17505208c40c9b9fba6be97f49b02edcdb
artvazVisolutions <138157347+artvazVisolutions@users.noreply.github.com>
2023-10-19 14:18:57 -0600

    Update DemoPosaBackendApplication.java

--- src/main/java/com/example/DemoPosaBackend/DemoPosaBackendApplication.java
+++ src/main/java/com/example/DemoPosaBackend/DemoPosaBackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.DemoPosaBackend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,28 +8,13 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DemoPosaBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoPosaBackendApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("/**");
-			}
-		};
-		return webMvcConfigurer;
-	}
-
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
-
 
 }
